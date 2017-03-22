a94d39073498bca7f42b2280d7f79cad486dca7b
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-03-22 19:25:55 +0200

    Material Design components added.
    Added mdl components: grid, header and buttons. material-styling.js file
    added

---
+++ Project/script/material-styling.js
@@ -0,0 +1,5 @@
+(function () {
+    $('.click-btn').addClass('mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent').css('background-color', '#FF9800');
+    $('#buttons-container-canvas .click-btn').css('background-color', '#FFB74D');
+    $('#sidebar-right .click-btn').css('background-color', '#FFB74D');
+})();   
